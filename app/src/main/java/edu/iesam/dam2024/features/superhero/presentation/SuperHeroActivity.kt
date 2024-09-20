6e2814a49bb3a18f9a54cdf66866101cfe2f3619
Chema <josem.perram@educa.jcyl.es>
2024-09-20 17:46:33 +0200

    Crear estructura de la arquitectura para SuperHero

---
+++ app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroActivity.kt
@@ -0,0 +1,16 @@
+package edu.iesam.dam2024.features.superhero.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import edu.iesam.dam2024.R
+
+class SuperHeroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_superhero)
+    }
+}
