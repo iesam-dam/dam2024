a8da200784be9cb082fc345f97942964a1ad5fb0
Chema <josem.perram@educa.jcyl.es>
2024-09-25 16:20:04 +0200

    Añadida funcionalidad para visualizar un listado de superheroes.

--- app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroActivity.kt
@@ -1,16 +1,21 @@
 package edu.iesam.dam2024.features.superhero.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import edu.iesam.dam2024.R
+import edu.iesam.dam2024.features.superhero.domain.SuperHero
 
 class SuperHeroActivity : AppCompatActivity() {
+
+    private val superHeroFactory = SuperHeroFactory()
+    private val viewModel = superHeroFactory.buildViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_superhero)
+
+        val superHeros: List<SuperHero> = viewModel.viewCreated()
+        Log.d("@dev", superHeros.toString())
     }
 }
