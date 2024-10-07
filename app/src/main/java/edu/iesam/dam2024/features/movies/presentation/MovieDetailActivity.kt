70476ba588b058e7dfbda5283ca32a3d37b7f28d
josem.perram <josem.perram@educa.jcyl.es>
2024-10-07 19:40:36 +0200

    Añadido detalle de Movie

--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieDetailActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieDetailActivity.kt
@@ -3,11 +3,13 @@ package edu.iesam.dam2024.features.movies.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import edu.iesam.dam2024.R
 import edu.iesam.dam2024.app.extensions.loadUrl
@@ -21,15 +23,31 @@ class MovieDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
-
         movieFactory = MovieFactory(this)
         viewModel = movieFactory.buildMovieDetailViewModel()
-
         getMovieId()?.let { movieId ->
-            viewModel.viewCreated(movieId)?.let { movie ->
-                bindData(movie)
+            viewModel.viewCreated(movieId)
+        }
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        val movieObserver = Observer<MovieDetailViewModel.UiState> { uiState ->
+            uiState.movie?.let {
+                bindData(it)
+            }
+            uiState.errorApp?.let {
+                //pinto el error
+            }
+            if (uiState.isLoading) {
+                //muestro el cargando...
+                Log.d("@dev", "Cargando...")
+            } else {
+                //oculto el cargando...
+                Log.d("@dev"," Cargado ...")
             }
         }
+        viewModel.uiState.observe(this, movieObserver)
     }
 
     private fun getMovieId(): String? {
--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieDetailViewModel.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieDetailViewModel.kt
@@ -1,14 +1,33 @@
 package edu.iesam.dam2024.features.movies.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.iesam.dam2024.app.domain.ErrorApp
 import edu.iesam.dam2024.features.movies.domain.GetMovieUseCase
 import edu.iesam.dam2024.features.movies.domain.Movie
+import edu.iesam.dam2024.features.movies.presentation.MoviesViewModel.UiState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MovieDetailViewModel(
     private val getMovieUseCase: GetMovieUseCase
 ) : ViewModel() {
 
-    fun viewCreated(movieId: String): Movie? {
-        return getMovieUseCase.invoke(movieId)
+    private val _uiState = MutableLiveData<UiState>()
+    val uiState : LiveData<UiState> = _uiState
+
+    fun viewCreated(movieId: String){
+        viewModelScope.launch(Dispatchers.IO) {
+            val movie = getMovieUseCase.invoke(movieId)
+            _uiState.postValue(UiState(movie = movie))
+        }
     }
+
+    data class UiState(
+        val isLoading: Boolean = false,
+        val errorApp: ErrorApp? = null,
+        val movie : Movie? = null
+    )
 }
