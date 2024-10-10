d6959c0bf20d774cce785c7021a36df8437d834c
josem.perram <josem.perram@educa.jcyl.es>
2024-10-10 20:34:49 +0200

    Añadido superhero con navegación

---
+++ app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroDetailFragment.kt
@@ -0,0 +1,32 @@
+package edu.iesam.dam2024.features.superhero.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import edu.iesam.dam2024.databinding.FragmentMovieDetailBinding
+import edu.iesam.dam2024.databinding.FragmentSuperheroDetailBinding
+
+class SuperHeroDetailFragment : Fragment() {
+
+    private var _binding: FragmentSuperheroDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val superHeroArgs: SuperHeroDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSuperheroDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        superHeroArgs.superHeroId
+    }
+}
--- app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroListFragment.kt
+++ app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroListFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import edu.iesam.dam2024.databinding.FragmentMoviesBinding
 import edu.iesam.dam2024.databinding.FragmentSuperheroListBinding
+import edu.iesam.dam2024.features.superhero.domain.SuperHero
 
 class SuperHeroListFragment : Fragment() {
 
@@ -35,9 +37,51 @@ class SuperHeroListFragment : Fragment() {
     }
 
     private fun setupObserver(){
-        val observer = Observer<SuperHeroListViewModel.UiState>{
+        val observer = Observer<SuperHeroListViewModel.UiState>{ uiState ->
             //código de respuesta
+            uiState.superHeroes?.let { superHeroes ->
+                bindData(superHeroes)
+            }
+
+            uiState.errorApp?.let {
+                //Pinto el error
+            } ?: run {
+                //Ocultar el error
+            }
+
+            if (uiState.isLoading){
+                //Muestro cargando...
+            } else {
+                //Oculto el cargando...
+            }
         }
         viewModel.uiState.observe(viewLifecycleOwner, observer)
+    }
+
+    private fun bindData(superHeroes: List<SuperHero>){
+        binding.apply {
+            superhero1.apply {
+                text = superHeroes[0].name
+                setOnClickListener {
+                    navigateToDetails(superHeroes[0].id)
+                }
+            }
+
+            superhero2.text = superHeroes[1].name
+            superhero2.setOnClickListener {
+                navigateToDetails(superHeroes[1].id)
+            }
+
+            superhero3.text = superHeroes[2].name
+            superhero3.setOnClickListener {
+                navigateToDetails(superHeroes[2].id)
+            }
+        }
+    }
+
+    private fun navigateToDetails(superHeroId: String){
+        findNavController().navigate(SuperHeroListFragmentDirections
+            .actionFromSuperheroToSuperheroDetail(superHeroId)
+        )
     }
 }
